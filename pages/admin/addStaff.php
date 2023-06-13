b6b3578f34b0093203a1f2434475d4ce92034622
Briskye <133975189+Briskye@users.noreply.github.com>
2023-06-13 23:04:10 +0800

    edit,delete,reset button

--- pages/admin/addStaff.php
+++ pages/admin/addStaff.php
@@ -25,11 +25,11 @@ include '../../assets/template/nav.php';
           <input class="searchBar form-control-lg mr-sm-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btnSearch btn btn-outline-success" type="submit">Search</button>
           <a class="btnAddStaff btn btn-outline-primary" href="#btnAdd">Add Staff</a>
-          <p id="response"></p> //@martin pa css eto yung notif sa html kung success yung pag add sa db
+          <p id="response"></p> //@david ayaw malagyan css pre.
         </form>
       </nav>
       <div class="table-responsive">
-        <table class="table table-hover">
+        <table class="table table-bordered">
           <tr>
             <th scope="col">Image</th>
             <th scope="col">Full Name</th>
@@ -40,9 +40,22 @@ include '../../assets/template/nav.php';
             <th scope="col">Action</th>
           </tr>
 
-          <tr>
+      <tr>
 
-          </tr>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>David</td>
+            <td>mdc</td>
+            <td>mdc</td>
+            <td>mdc</td>
+            <td>
+              <a class="btn btn-sm btn-primary" href="#btnEdit">Edit</a>
+              <a class="btn btn-sm btn-danger" href="">Delete</a>
+              <a class="btn btn-sm btn-dark" href="">Reset Password</a>
+            </td>
+      </tr>
+
         </table>
       </div>
       <div class="overlay" id="btnAdd">
@@ -64,6 +77,31 @@ include '../../assets/template/nav.php';
                 <label class="d-flex">E-mail</label>
                 <input class="d-flex" placeholder="Enter E-mail" type="text" id="email">
                 <input class="btn btn-success" value="Submit" onclick="submitData('insert')">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="overlay" id="btnEdit">
+        <div class="wrapper">
+          <h2>Edit Details</h2><a class="close" href="#">&times;</a>
+          <div class="content">
+            <div class="container">
+              <form>
+                <label class="d-flex">Last Name</label>
+                <input class="d-flex" placeholder="Enter New Last Name" type="text" id="lastName">
+                <label class="d-flex">First Name</label>
+                <input class="d-flex" placeholder="Enter New Full Name" type="text" id="firstName">
+                <label class="d-flex">Middle Name</label>
+                <input class="d-flex" placeholder="Enter New Middle Name" type="text" id="middleName">
+                <label class="d-flex">Position</label>
+                <input class="d-flex" placeholder="Enter New Position" type="text" id="position">
+                <label class="entCont d-flex">Contact #</label>
+                <input class="entContact d-flex" placeholder="Enter New Contact no." type="number" id="contactNumber">
+                <label class="d-flex">E-mail</label>
+                <input class="d-flex" placeholder="Enter New E-mail" type="text" id="email">
+                <input class="btn btn-success" value="Submit">
               </form>
             </div>
           </div>
