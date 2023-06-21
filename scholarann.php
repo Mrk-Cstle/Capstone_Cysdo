4ba181e7f4d45431973d74d012d036a4b2657ec5
Eremingming <akosijer.aj@gmail.com>
2023-06-21 17:39:08 +0800

    updated na yung announcement tab

--- scholarann.php
+++ scholarann.php
@@ -16,11 +16,8 @@
         #newsList {
             display: flex;
             flex-direction: row;
-            width: 100%;
-            gap: 20px;
             justify-content: center;
             margin-top: 50px;
-
         }
 
         .newsFormat {
@@ -29,22 +26,26 @@
             border: 1px solid black;
             width: 400px;
             height: 500px;
+            margin: 0 10px;
+            align-items: center;
+            text-align: center;
         }
 
         .newsFormat img {
             width: 100%;
             height: 300px;
-            text-align: center;
-            margin: 0px;
+            object-fit: cover;
+            margin: 0;
+        }
 
-
+        .newsFormat h2 {
+            margin-top: 10px;
         }
 
         .newsFormat a {
             border: 1px solid black;
             text-decoration: none;
             padding: 10px;
-
         }
     </style>
 </head>
@@ -53,28 +54,21 @@
     <?php
     include 'assets/template/homeNavigation.php';
     ?>
-    <h1>News and Updates</h1>
+
+    <h1>News & Updates</h1>
+
     <div id="newsList">
         <div class="newsFormat">
             <img src="assets/image/No_Image_Available.jpg" />
-            <h2>Approved New Scholar</h2>
-            <a href="#">View</a>
+            <h2>Scholar Announcement</h2>
+            <a href="sann.php">View</a>
         </div>
         <div class="newsFormat">
             <img src="assets/image/No_Image_Available.jpg" />
-            <h2>CYSDO Scholars List</h2>
-            <a href="#">View</a>
+            <h2>Applicant Announcement</h2>
+            <a href="appann.php">View</a>
         </div>
-        <div class="newsFormat">
-            <img src="assets/image/No_Image_Available.jpg" />
-            <h2>Announcement</h2>
-            <a href="scholarann.php">View</a>
-        </div>
-        <div class="newsFormat">
-            <img src="assets/image/No_Image_Available.jpg" />
-            <h2>Events</h2>
-            <a href="#">View</a>
-        </div>
+    </div>
 </body>
 
 </html>
