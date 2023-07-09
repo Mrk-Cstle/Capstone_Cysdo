4f8afced5e7574674b8a8452ce710769e9862861
Eremingming <akosijer.aj@gmail.com>
2023-07-09 14:20:23 +0800

    Push ko muna
    Gumagana naman na lahat yung delete lang ayaw mag refresh

--- pages/admin/adminHome.php
+++ pages/admin/adminHome.php
@@ -88,18 +88,16 @@
             </ul>
 
 
-<div class="table-data">
-                
+            <div class="table-data">
     <div class="todo">
-    <div class="head">
-        <h3>Todos</h3>
-
-        <a class="btnAddTodo bx bx-plus dropdown-toggle-split text-black" type="button" data-bs-target="#addTodo" data-bs-toggle="modal" data-bs-whatever="@addTo-do"></a>
-        <div class="modal fade" id="addTodo" aria-labelledby="addTodoLabel" aria-expanded="false">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h2 class="modal-title fs-5" id="addTodoLabel">New Todo</h2>
+        <div class="head">
+            <h3>Todos</h3>
+            <a class="btnAddTodo bx bx-plus dropdown-toggle-split text-black" type="button" data-bs-target="#addTodo" data-bs-toggle="modal" data-bs-whatever="@addTo-do"></a>
+            <div class="modal fade" id="addTodo" aria-labelledby="addTodoLabel" aria-expanded="false">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h2 class="modal-title fs-5" id="addTodoLabel">New Todo</h2>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
@@ -110,148 +108,139 @@
                                 </div>
                             </form>
                         </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Add</button>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" id="addButton">Add</button>
                         </div>
+                    </div>
                 </div>
             </div>
+            <i class="bx bx-filter" onclick="toggleTodoList()"></i>
         </div>
-        <i class="bx bx-filter" onclick="toggleTodoList()"></i>
-    </div>
-    <ul class="todo-list">
-        <?php
-        // Fetch todos from the database
-        $server = "localhost";
-        $username = "root";
-        $password = "";
-        $db = "cysdo";
+        <ul class="todo-list">
+            <?php
+            // Fetch todos from the database
+            $server = "localhost";
+            $username = "root";
+            $password = "";
+            $db = "cysdo";
 
-        // Create a database connection
-        $conn = new mysqli($server, $username, $password, $db);
+            // Create a database connection
+            $conn = new mysqli($server, $username, $password, $db);
 
-        // Check the connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        // SQL query to fetch todos
-        $sql = "SELECT * FROM todos";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $completed = $row['completed'] ? 'completed' : 'not-completed';
-                $todoId = $row['id'];
-                $todoText = $row['todo_text'];
-        ?>
-                
-            <li class="<?php echo $completed; ?>">
-                <p><?php echo $todoText; ?></p>
-                <div class="dropdown-center">
-                    <a class="bx bx-dots-vertical-rounded text-black" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <span class="visually-hidden">Toggle Dropdown</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <button class="dropdown-item" type="button">Mark As Done</button>
-                        <button class="dropdown-item" type="button">Delete</button>
-                    </ul>
-                </div>
-            </li>
-            
-        <?php
+            // Check the connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
             }
-        } else {
-            echo "No todos found.";
-        }
 
-        $conn->close();
-        ?>
-    </ul>
+            // SQL query to fetch todos
+            $sql = "SELECT * FROM todos";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $completed = $row['completed'] ? 'completed' : 'not-completed';
+                    $todoId = $row['id'];
+                    $todoText = $row['todo_text'];
+            ?>
+
+                    <li class="<?php echo $completed; ?>" data-id="<?php echo $todoId; ?>">
+                        <p><?php echo $todoText; ?></p>
+                        <div class="dropdown-center">
+                            <a class="bx bx-dots-vertical-rounded text-black" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="visually-hidden">Toggle Dropdown</span>
+                            </a>
+                            <ul class="dropdown-menu" data-id="<?php echo $todoId; ?>">
+                                <button class="dropdown-item" type="button" onclick="markAsDone(<?php echo $todoId; ?>)">Mark As Done</button>
+                                <button class="dropdown-item" type="button" onclick="deleteTodoAjax(<?php echo $todoId; ?>)">Delete</button>
+                            </ul>
+                        </div>
+                    </li>
+
+            <?php
+                }
+            } else {
+                echo "No todos found.";
+            }
+
+            $conn->close();
+            ?>
+        </ul>
+    </div>
 </div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    const exampleModal = document.getElementById('addTodo')
-if (exampleModal) {
-  exampleModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
-    const button = event.relatedTarget
-    // Extract info from data-bs-* attributes
-    const recipient = button.getAttribute('data-bs-whatever')
-    // If necessary, you could initiate an Ajax request here
-    // and then do the updating in a callback.
-
-    // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-    modalTitle.textContent = `New message to ${recipient}`
-    modalBodyInput.value = recipient
-  })
-}
-</script>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script>
-$(document).ready(function() {
-    // Function to initialize event handlers
-    function initializeEventHandlers() {
-        // Add click event handler to the dots icon
-        $('.todo-list').on('click', '.bx-dots-vertical-rounded', function() {
-            var todoId = $(this).closest('li').data('id');
-            openPopup(todoId);
-        });
-    }
-
-    // Add a new todo
-    $('.head i.bx-plus').click(function() {
-        var todoText = prompt('Enter a new todo:');
-        if (todoText) {
-            $.ajax({
-                url: 'addTodo.php',
-                type: 'POST',
-                data: { todo_text: todoText },
-                dataType: 'json',
-                success: function(response) {
-                    if (response.status === 'success') {
-                        var todoId = response.todo_id;
-                        var newTodo = $('<li>').addClass('not-completed')
-                            .data('id', todoId)
-                            .append($('<p>').text(todoText))
-                            .append(
-                                $('<i>').addClass('bx bx-dots-vertical-rounded').attr('onclick', 'openPopup(' + todoId + ')')
-                            )
-                            .append(
-                                $('<div>').attr('id', 'popup-' + todoId).addClass('popup')
-                                    .append(
-                                        $('<button>').text('Mark as Done').attr('onclick', 'markAsDone(' + todoId + ')')
-                                    )
-                                    .append(
-                                        $('<button>').text('Delete').attr('onclick', 'deleteTodoAjax(' + todoId + ')')
-                                    )
-                            )
-                            .append(
-                                $('<div>').attr('id', 'overlay-' + todoId).addClass('overlay').attr('onclick', 'closePopup(' + todoId + ')')
-                            );
-                        $('.todo-list').append(newTodo);
-                        initializeEventHandlers(); // Re-initialize event handlers for the new todo
-                    } else {
-                        console.log(response.message);
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.log('Error: ' + error);
-                }
+    $(document).ready(function () {
+        // Function to initialize event handlers
+        function initializeEventHandlers() {
+            // Add click event handler to the dots icon
+            $('.todo-list').on('click', '.bx-dots-vertical-rounded', function () {
+                var todoId = $(this).closest('li').data('id');
+                openPopup(todoId);
             });
         }
+
+        // Add a new todo
+        $('#addButton').click(function () {
+            var todoText = $('#addNew').val().trim();
+            if (todoText !== '') {
+                $.ajax({
+                    url: 'addTodo.php',
+                    type: 'POST',
+                    data: {
+                        todo_text: todoText
+                    },
+                    dataType: 'json',
+                    success: function (response) {
+                        if (response.status === 'success') {
+                            var todoId = response.todo_id;
+                            var newTodo = $('<li>').addClass('not-completed')
+                                .data('id', todoId)
+                                .append($('<p>').text(todoText))
+                                .append(
+                                    $('<div>').addClass('dropdown-center')
+                                        .append(
+                                            $('<a>').addClass('bx bx-dots-vertical-rounded text-black').attr('data-bs-toggle', 'dropdown')
+                                                .append(
+                                                    $('<span>').addClass('visually-hidden').text('Toggle Dropdown')
+                                                )
+                                        )
+                                        .append(
+                                            $('<ul>').addClass('dropdown-menu').attr('data-id', todoId)
+                                                .append(
+                                                    $('<button>').addClass('dropdown-item').attr('type', 'button').text('Mark As Done')
+                                                        .on('click', function () {
+                                                            markAsDone(todoId);
+                                                        })
+                                                )
+                                                .append(
+                                                    $('<button>').addClass('dropdown-item').attr('type', 'button').text('Delete')
+                                                        .on('click', function () {
+                                                            deleteTodoAjax(todoId);
+                                                        })
+                                                )
+                                        )
+                                );
+                            $('.todo-list').append(newTodo);
+                            $('#addTodo').modal('hide');
+                            $('#addNew').val('');
+                            initializeEventHandlers(); // Re-initialize event handlers for the new todo
+                        } else {
+                            console.log(response.message);
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.log('Error: ' + error);
+                    }
+                });
+            }
+        });
+
+        // Initialize event handlers
+        initializeEventHandlers();
     });
-
-    // Initialize event handlers
-    initializeEventHandlers();
-});
-
-
 
     function openPopup(todoId) {
         $('#popup-' + todoId).show();
@@ -264,70 +253,76 @@ $(document).ready(function() {
     }
 
     function markAsDone(todoId) {
-    // Update the UI immediately
-    var listItem = $('#popup-' + todoId).closest('li');
-    var isCompleted = listItem.hasClass('completed');
+        // Update the UI immediately
+        var listItem = $('li[data-id="' + todoId + '"]');
+        var isCompleted = listItem.hasClass('completed');
 
-    if (isCompleted) {
-        listItem.removeClass('completed');
-        listItem.addClass('not-completed');
-    } else {
-        listItem.removeClass('not-completed');
-        listItem.addClass('completed');
+        if (isCompleted) {
+            listItem.removeClass('completed');
+            listItem.addClass('not-completed');
+        } else {
+            listItem.removeClass('not-completed');
+            listItem.addClass('completed');
+        }
+
+        closePopup(todoId);
+
+        // Send AJAX request to update the completed status in the database
+        $.ajax({
+            url: 'updateTodo.php',
+            type:'POST',
+            data: {
+                todo_id: todoId,
+                completed: isCompleted ? 0 : 1
+            }, // Toggle the completed status
+            success: function (response) {
+                // Todo status updated successfully
+                // You can perform any other actions if needed
+            },
+            error: function (xhr, status, error) {
+                // Error updating todo status
+                console.log(xhr.responseText);
+                // Revert the UI back to the original state if an error occurs
+                if (isCompleted) {
+                    listItem.removeClass('not-completed');
+                    listItem.addClass('completed');
+                } else {
+                    listItem.removeClass('completed');
+                    listItem.addClass('not-completed');
+                }
+            }
+        });
     }
 
-    closePopup(todoId);
-
-    // Send AJAX request to update the completed status in the database
-    $.ajax({
-        url: 'updateTodo.php',
-        type: 'POST',
-        data: { todo_id: todoId, completed: isCompleted ? 0 : 1 }, // Toggle the completed status
-        success: function(response) {
-            // Todo status updated successfully
-            // You can perform any other actions if needed
-        },
-        error: function(xhr, status, error) {
-            // Error updating todo status
-            console.log(xhr.responseText);
-            // Revert the UI back to the original state if an error occurs
-            if (isCompleted) {
-                listItem.removeClass('not-completed');
-                listItem.addClass('completed');
-            } else {
-                listItem.removeClass('completed');
-                listItem.addClass('not-completed');
-            }
-        }
-    });
-}
-
-function toggleTodoList() {
+    function toggleTodoList() {
         $('.todo-list').toggle();
     }
 
-
     function deleteTodoAjax(todoId) {
-        if (confirm('Are you sure you want to delete this task?')) {
-            $.ajax({
-                url: 'deleteTodo.php',
-                type: 'POST',
-                data: {
-                    delete: todoId
-                },
-                success: function(response) {
-                    // Todo deleted successfully
-                    // You can update the UI or perform any other actions
-                    $('#popup-' + todoId).closest('li').remove();
-                },
-                error: function(xhr, status, error) {
-                    // Error deleting todo
-                    console.log(xhr.responseText);
-                }
-            });
-        }
-    }
+  if (confirm('Are you sure you want to delete this task?')) {
+    $.ajax({
+      url: 'deleteTodo.php',
+      type: 'POST',
+      data: {
+        delete: todoId
+      },
+      success: function (response) {
+        // Todo deleted successfully
+        // You can update the UI or perform any other actions
+        $('#popup-' + todoId).closest('li').remove();
+      },
+      error: function (xhr, status, error) {
+        // Error deleting todo
+        console.log(xhr.responseText);
+      }
+    });
+  }
+}
+
+    
 </script>
+
+
 
 </main>
 </section>
