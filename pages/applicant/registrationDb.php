c98f45f181e76a0a2a895ceab837dd491c600acc
Briskye <133975189+Briskye@users.noreply.github.com>
2023-09-30 21:15:04 +0800

    printable form asfk

--- pages/applicant/registrationDb.php
+++ pages/applicant/registrationDb.php
@@ -81,7 +81,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         move_uploaded_file($_FILES["signPic"]["tmp_name"], $signPicPath)
     ) {
         // Both images were successfully uploaded
-        echo "Both images were successfully uploaded.";
+        echo "";
 
 
         $query = "INSERT INTO registration (fullName, lastName, firstName,middleName,gender, civilStatus, voter, birthDate,birthPlace, citizenship, houseAddress, streetAddress, barangayAddress, contactNum1, contactNum2, pic2x2, signaturePic, schoolName, schoolAddress, schoolType, course, yearLevel, fatherName, fatherStatus,fatherAddress, fatherContact, fatherOccupation, fatherEduc, motherName, motherStatus, motherAddress, motherContact, motherOccupation, motherEduc, guardianName, guardianAddress, guardianContact, guardianOccupation, guardianEduc, sizeFamily, annualGross,  sibling1,sibling2,sibling3,sibling4,sibling5,sibling6 ) VALUES ( '$fullName','$lastName' , '$firstName', '$middleName', '$gender', '$civilStatus', '$registeredVoter', '$birthDate','$birthPlace', '$citizenship', '$addressNum', '$addressStreet', '$addressBarangay', '$contactNumber1', '$contactNumber2', '$imageName', '$signName', '$schoolName', '$schoolAddress', '$schoolType', '$course', '$currentLevel', '$fatherName', '$father', '$fatherAddress', '$fatherNumber', '$fatherOccupation',  '$fEducAttainment', '$motherName', '$mother', '$motherAddress', '$motherNumber', '$motherOccupation', '$mEducAttainment', '$guardianName', '$guardianAddress', '$guardianNumber', '$guardianOccupation', '$gEducAttainment', '$sizeFamily', '$familyIncome', '$sibling1', '$sibling2', '$sibling3', '$sibling4', '$sibling5', '$sibling6')";
@@ -119,7 +119,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         td {
             border-collapse: collapse;
             border: 1px solid black;
-            padding: 20px;
+            padding: 12px;
             text-align: center;
         }
 
@@ -150,6 +150,70 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             align-items: center;
             flex-direction: column;
             width: 100%;
+            padding: 20px;
+        }
+
+        @media screen {
+
+            body {
+                margin-bottom: 2em;
+                margin-top: 10px;
+                padding: 40;
+            }
+
+        }
+
+/* print styles */
+        @media print {
+
+            body {
+                margin: 0;
+                color: #000;
+                background-color: #fff;
+                padding: 0;
+            }
+            #instruction {
+                display: none;
+            }
+        }
+
+        header, footer, aside, nav, form, iframe, .menu, .hero, .adslot {
+            display: none;
+        }
+
+        @media print {
+
+            .page-break {
+                page-break-before: always;
+            }
+            body{
+                font-size: 10px;
+                font-family: Arial, sans-serif;
+            }
+
+            td, tr, th {
+                font-size: 10px;
+            }
+
+            .headerz {
+                font-size: 10px;
+            }
+
+            #instruction {
+                display: none;
+            }
+            .ControlNm {
+                margin-top: 2px;
+                font-size: 8px;
+                margin-bottom: 0;
+            }
+        }
+
+        .signature {
+            text-align: center;
+            font-weight: 700;
+            flex-direction: column;
+            font-family: sans-serif;
         }
 
         header {
@@ -172,10 +236,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             width: 100px;
             height: 100px;
             align-items: start;
+            margin-top: 5px;
         }
 
         #picture2x2 img {
-            width: 120px;
+            width: 200px;
             height: auto;
         }
 
@@ -192,7 +257,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         #headerCenter p {
-            font-size: 15px;
+            font-size: 14px;
         }
 
         #instruction {
@@ -201,25 +266,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             justify-content: center;
             align-items: flex-start;
             width: 70%;
+            font-size: 14;
+        }
+
+        @media print {
+            #headerCenter p {
+            font-size: 10px;
+            }
+
+            #instruction {
+                margin: 0;
+                padding: 0;
+                font-size: 10px;
+            }
+
+            #picture2x2 img {
+                width: 120px;
+                height: auto;
+            }
+
+            #headerLogo img {
+                width: 80px;
+                height: auto;
+            }
+
         }
 
         @media (max-width: 710px) {
             
             .tablez {
-            width: 50%;
+            width: 100%;
             }
         }
+
+        
     </style>
 </head>
 
 <body>
-<div class="container">
+<div class="container-fluid">
     <header>
 
 
         <div id="headerLogo">
-            <img src="../../assets/image/San-Jose-del-Monte-Official-Seal.png" alt="San Jose Logo" />
-            <img src="../../assets/image/CysdoLogo.png" alt="CYSDO LOGO" />
+            <img class="img-fluid" src="../../assets/image/San-Jose-del-Monte-Official-Seal.png" alt="San Jose Logo" />
+            <img class="img-fluid" src="../../assets/image/CysdoLogo.png" alt="CYSDO LOGO" />
         </div>
 
         <div id="headerCenter">
@@ -253,14 +344,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     </header>
     <div id="instruction">
-        <p>Instructions:</p>
-        <p>Please read the General and Documentary Requirements. Fill the required information.
+        <p>Instructions: <br/>
+            Please read the General and Documentary Requirements. Fill the required information.
             Do not leave an item blank. If item is not applicable, indicate "N/A".
         </p>
+    </div>
+    <div class="ControlNm">
         <p><strong> CONTROL NUMBER: ______________ </strong></p>
     </div>
+    
 
     <div class="tablez">
+        <div class="row">
         <table>
             <td colspan="6" class="headerz">PERSONAL INFO</td>
             <tr>
@@ -307,41 +402,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <th>Contact Number 2</th>
                 <th>Registered Voter?</th>
             </tr>
-        </table>
-        <table>
+        </div>
+        <div class="row">
         <td colspan="6" class="headerz">EDUCATIONAL BACKGROUND</td>
             <tr>
-                <th>School Name</th>
-                <th>School Address</th>
+                <th colspan="2">School Name</th>
+                <th colspan="3">School Address</th>
                 <th>School Type</th>
             </tr>
             <tr>
-                <td><?php echo "$schoolName" ?></td>
-                <td><?php echo "$schoolAddress" ?></td>
+                <td colspan="2"><?php echo "$schoolName" ?></td>
+                <td colspan="3"><?php echo "$schoolAddress" ?></td>
                 <td><?php echo "$schoolType" ?></td>
             </tr>
             <tr>
-                <th>School Name</th>
-                <th>School Address</th>
+                <th colspan="2">School Name</th>
+                <th colspan="3">School Address</th>
                 <th>School Type</th>
             </tr>
             <tr>
-                <td><?php echo "$course" ?></td>
+                <td colspan="2"><?php echo "$course" ?></td>
 
-                <td colspan="2"><?php echo "$currentLevel" ?></td>
+                <td colspan="3"><?php echo "$currentLevel" ?></td>
             </tr>
             <tr>
 
-                <th>Course/Strand</th>
-                <th colspan="2">Current Year Level</th>
+                <th colspan="2">Course/Strand</th>
+                <th colspan="3">Current Year Level</th>
+                <th></th>
 
             </tr>
-        </table>
-        <table>
+        </div>
+        <div class="row">
         <td colspan="6" class="headerz">FAMILY BACKGROUND</td>
             <tr>
                 <td></td>
-                <th>Father: <?php echo "$father" ?></th>
+                <th colspan="2">Father: <?php echo "$father" ?></th>
                 <th>Mother: <?php echo "$mother" ?></th>
                 <th>Guardian</th>
                 <th>Name of siblings</th>
@@ -349,7 +445,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </tr>
             <tr>
                 <th>Name</th>
-                <td><?php echo "$fatherName" ?></td>
+                <td colspan="2"><?php echo "$fatherName" ?></td>
                 <td><?php echo "$motherName" ?></td>
                 <td><?php echo "$guardianName" ?></td>
                 <td><?php echo "$sibling1" ?></td>
@@ -357,14 +453,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </tr>
             <tr>
                 <th>Address</th>
-                <td><?php echo "$fatherAddress" ?></td>
+                <td colspan="2"><?php echo "$fatherAddress" ?></td>
                 <td><?php echo "$motherAddress" ?></td>
                 <td><?php echo "$guardianAddress" ?></td>
                 <td><?php echo "$sibling2" ?></td>
             </tr>
             <tr>
                 <th>Contact No.</th>
-                <td><?php echo "$fatherNumber" ?></td>
+                <td colspan="2"><?php echo "$fatherNumber" ?></td>
                 <td><?php echo "$motherNumber" ?></td>
                 <td><?php echo "$guardianNumber" ?></td>
                 <td><?php echo "$sibling3" ?></td>
@@ -372,7 +468,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </tr>
             <tr>
                 <th>Occupation</th>
-                <td><?php echo "$fatherOccupation" ?></td>
+                <td colspan="2"><?php echo "$fatherOccupation" ?></td>
                 <td><?php echo "$motherOccupation" ?></td>
                 <td><?php echo "$guardianOccupation" ?></td>
                 <td><?php echo "$sibling4" ?></td>
@@ -380,7 +476,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </tr>
             <tr>
                 <th>Educational Attainment</th>
-                <td><?php echo "$fEducAttainment" ?></td>
+                <td colspan="2"><?php echo "$fEducAttainment" ?></td>
                 <td><?php echo "$mEducAttainment" ?></td>
                 <td><?php echo "$gEducAttainment" ?></td>
                 <td><?php echo "$sibling5" ?></td>
@@ -388,10 +484,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </tr>
             <tr>
                 <th colspan="2">Total Parents Annual Gross Income: <?php echo "$familyIncome" ?></th>
-                <th colspan="2">Size of Family: <?php echo "$sizeFamily" ?></th>
+                <th colspan="3">Size of Family: <?php echo "$sizeFamily" ?></th>
                 <td><?php echo "$sibling6" ?></td>
             </tr>
-        </table>
+      </div>
+      <div class="row">
+      <td colspan="6" class="headerz">I/We hereby certify that the information above are true and correct.</td>
+            <tr>
+                <td class="signature" rowspan="4" colspan="2">____________________________<br/>Applicant's Signature over Printed Name  </td>
+                <td class="signature" rowspan="4" colspan="2">____________________________<br/> Parent's / Guardian's Signature over Printed Name</td>
+                <td class="signature" rowspan="4" colspan="2">__________________</br> (Date Accomplished) </td>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+            <tr></tr>
+            <tr>
+                <td colspan="6" class="headerz">(Note: Fully accomplished form must be submitted to CYSDO.)</td>
+            </tr>
+            <tr>
+                <td colspan="6">_______________________________________________________________________________________________________________________________________ <br/>
+                Cut Here
+                </td>
+            </tr>
+      </div>
+      <table>
+      <div class="row">
+        <td rowspan="6" colspan="6">(Student's Copy)<br/><p class="text-danger"> MGA PAALALA: </p><br/><p>1. Filipino, may asawa o wala at kailangang naninirahan sa Lungsod ng San Jose Del Monte sa loob ng tatlo (3) o mahigit pang taon, nakapagtapos ng sekondarya sa alin mang paaralan sa Lungsod, pribado man o pampubliko. Kung hindi naman nakapagtapos sa Lungsod ay kinakailangang residente nito sa nakalipas na limang (5) taon. <br/></p>
+            <p>2. Hindi lalagpas ng 30 taon ang edad sa araw ng paghahain ng aplikasyon.<br/><p>
+            <p>3. Hindi bababa sa 76% (passing grade) ang general average sa huling taon sa Senior High School.<br/></p>
+            <p>4. Kailangang mapanatili ang 15 enrolled units lakip ang Curriculum or SYLLABUS per semester maliban sa mga magtatapos o graduating students.<br/></p>
+            <p>5. Maaring magpatala sa anumang kursong kanilang nanaisin maging Degree Course (4-year o 5-year courses). o Non-Degree Course (3-year o 2-year courses).<br/></p>
+            <p>6. Ang inyong aplikasyon ay hindi garantiya na kayo ay tanggap na, kailangang sumailalim at pumasa sa mga sumusunod:<br/> a. Qualifying Examination  b. Interview  c. Community Investigation<br/></p>
+            <p>7. May mabuting asal (Certificate of Good Moral)<br/></p>
+            <p>8. Tinatangkilik lamang ang magulang na may pinagsamang kita na hindi hihigit sa P25,000.00 kada buwan o P300,000.00 bawat taon.<br/></p>
+            <p>9. Isa lamang sa bawat pamilya ang maaaring mabigyan ng Educational Assistance, sa pasubali na kung nakatapos na sa kolehiyo ang isang iskolar ay doon pa lang maaaring magkaroon ng pagkakataon na maging iskolar ang isa sa kanyang kapatid, maliban din naman kung kambal o triplet o quadruplet ay bibigyan ng exemption sa aytem na ito.<br/></p>
+            <p>10. Walang tinatanggap na educational assistance o scholarship sa kahit anong ahensya o organisasyon ng Gobyerno at iba pang Lokal na Pamahalaan.<br/></p>
+            <p>11. Ang mga mag-aaral ng City College of San Jose Del Monte (CCSJDM) at Bulacan State University-Sarmiento Campus (BSU-SC) o kahit anong State University o College sa loob ng Lungsod ng San Jose Del Monte na sumasailalim sa UNIFAST – Free Higher Education Program ay HINDI NA maaring maging benepisyaryo ng City Educational Assistance Program (CEAP).<br/></p>
+        </td>
+      </div>
+      </table>
+      <table>
+      <div class="row">
+        <th colspan="2" rowspan="3"><img src="../../assets/image/1x1.jpg" style="width: 200px; height: auto;"></th>
+        <td colspan="3"><img src="../../assets/image/examStub.png" style="width: 400px; height: auto; space-between: none;"></td>
+        
+        <tr class="">
+            <td colspan="5" rowspan="5" >
+            <p class="float-start">Control Number:_________________</p>
+            <p style="font-family: sans-serif;">Date Filling:_________________<br/></p>
+            <p><img src="../../assets/image/RisingCity.png" style="width: 80px; height: auto; float: end;" class="float-end"></p>
+            <p class="float-start" style="font-family: sans-serif;">Name of Applicant:________________________________<br/></p>
+            <p style="font-family: sans-serif;">Examination Date & Venue:________________________________<br/></p><br/>
+            <p class="float-start" style="font-family: sans-serif;">Signature:________________________________</p>
+            <p class="float-start" style="font-family: sans-serif;">Received by:________________________________</p>
+            
+            
+            </td>
+        </tr>
+        
+      </div>
+      </table>
     </div>
 </div>
 </body>
