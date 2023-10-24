f1de6faa90c912f438c855d140caabb2541a32c7
Mrk-Cstle <castillo.markdavid64@gmail.com>
2023-10-25 02:31:25 +0800

    Update scholarList.php

--- pages/admin/action/scholarList.php
+++ pages/admin/action/scholarList.php
@@ -2,7 +2,7 @@
 include '../../include/selectDb.php';
 include '../../include/dbConnection.php';
 
-$recordsPerPage = 1; // Change this to your desired value
+$recordsPerPage = 10; // Change this to your desired value
 
 // Initialize page number
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
@@ -15,7 +15,7 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        ?>
+?>
         <tr>
             <th class="user_id" scope="row"><?php echo $row['scholar_id']; ?></th>
             <td><?php echo $row['full_name']; ?></td>
@@ -28,7 +28,7 @@ if (mysqli_num_rows($result) > 0) {
             <td class="hidden-cell"><?php echo $row['first_name']; ?></td>
             <td class="hidden-cell"><?php echo $row['middle_name']; ?></td>
         </tr>
-        <?php
+<?php
     }
 
     // Calculate total number of rows for the search query
@@ -41,7 +41,7 @@ if (mysqli_num_rows($result) > 0) {
     if ($totalPages > 1) {
         $paginationHTML .= '<ul class="pagination">';
         $paginationHTML .= '<li class="page-item ' . ($page == 1 ? 'disabled' : '') . '"><a class="page-link pagination-button" href="#" data-page="' . ($page - 1) . '">Previous</a></li>';
-        
+
         // Add the current page count
         $paginationHTML .= '<li class="page-item disabled"><span class="page-link">Page ' . $page . ' of ' . $totalPages . '</span></li>';
 
